ae9a0e0af3bb8fc887e54846879d163587c48e39
Fanathing <mmybonnie@gamil.com>
2025-09-18 14:57:03 +0900

    Feat: POST 요청 발생 시 lists배열에 입력값을 파싱하여 추가 및 해당 list.html에서 넌적스로  lists를 순회하며 li및 p 생성

--- server.js
+++ server.js
@@ -5,28 +5,42 @@ const path = `${__dirname}/views/boards`;
 
 app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "html");
-nunjucks.configure("view", {
+nunjucks.configure("views", {
     express: app
 })
 
-const lists = [
-    {
-        id: 1,
-        userId: "",
-        content: ""
-    }
-]
+
+const now = new Date();
+const date = `${now.getFullYear()}/${now.getMonth()+1}/${now.getDate()}`
+
+const lists = [];
+
 
 app.get("/",(req, res) => {
     res.sendFile(`${path}/main.html`)
 })
 
 app.get("/boards",(req, res) => {
-    res.sendFile(`${path}/list.html`)
+    res.render("boards/list.html", {
+        lists
+    })
 })
 
 app.get("/boards/create",(req, res) => {
     res.sendFile(`${path}/create.html`)
+})
+
+app.post("/boards/create", (req, res) => {
+    
+    lists.push({
+        ...req.body,
+        id: lists.length +1,
+        date: date
+    });
+
+    console.log(lists);
+    
+    res.redirect("/boards")
 })
 
 app.listen(9898,() => {
