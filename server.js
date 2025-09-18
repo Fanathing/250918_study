7ad7a67dcf558301fc8236d30740eb8c9a5d2c9c
Fanathing <mmybonnie@gamil.com>
2025-09-18 14:23:19 +0900

    Feat: GET 요청에 따라 list.html 및 create.html 분기처리 server.js에서 body-parser 기능 활성화

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 const express = require("express");
+const nunjucks = require("nunjucks");
 const app = express();
 const path = `${__dirname}/views/boards`;
+
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "html");
+nunjucks.configure("view", {
+    express: app
+})
+
+const lists = [
+    {
+        id: 1,
+        userId: "",
+        content: ""
+    }
+]
 
 app.get("/",(req, res) => {
     res.sendFile(`${path}/main.html`)
 })
 
+app.get("/boards",(req, res) => {
+    res.sendFile(`${path}/list.html`)
+})
+
+app.get("/boards/create",(req, res) => {
+    res.sendFile(`${path}/create.html`)
+})
+
 app.listen(9898,() => {
+
     console.log("...서버는 켜졌다!");
 })
